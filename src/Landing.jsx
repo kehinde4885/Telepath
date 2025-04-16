8012a488b216989d831d4177e5e3c8d38501f0ab
kehinde4885 <kehindemalagu@gmail.com>
2025-04-16 12:09:16 +0100

    Added Desktop Header

---
+++ src/Landing.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+
+const Landing = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="hero" className="h-dvh bg-black">
+
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Landing;
