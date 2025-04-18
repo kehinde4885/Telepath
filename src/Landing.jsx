c545ef82212c8150234abf47a5208e64eeac97ea
kehinde4885 <kehindemalagu@gmail.com>
2025-04-18 16:38:55 +0100

    Added info sections

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
+import { Button2, Button3 } from "./components/Button";
+import Button from "./components/Button";
 
 const Landing = () => {
   return (
@@ -7,28 +9,10 @@ const Landing = () => {
       <Header />
       <main className="">
         {/* Stacking Context Created on Section Element */}
+        {/* Sections */}
         <section id="hero" className="relative z-0 h-dvh bg-black">
           <div className="absolute bottom-10 flex w-full">
-            <button className="group/cta relative mx-auto scale-[1.01] rounded-5xl border-2 border-transparent font-mono text-sm text-black uppercase outline-0 transition-colors focus-visible:border-lime">
-              <div className="absolute top-0 -z-10 h-full w-full rounded-5xl bg-white transition-all duration-500 group-hover/cta:scale-105 group-hover/cta:blur-[2px] hover:bg-black"></div>
-              <div className="flex h-10 items-center gap-2 px-4 text-black">
-                <span>watch film</span>
-                <span className="">
-                  <svg
-                    // width="17"
-                    // height="17"
-                    viewBox="0 0 17 17"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="aspect-square w-[17px]">
-                    <path
-                      d="M14.5001 8.89665L3.16675 15.5633V2.22998L14.5001 8.89665Z"
-                      fill="currentcolor"
-                    />
-                  </svg>
-                </span>
-              </div>
-            </button>
+            <Button2 text="watch film" />
           </div>
         </section>
 
@@ -39,34 +23,227 @@ const Landing = () => {
               For Chords
             </h2>
 
-            <a
-              href="#"
-              className="group/cta relative scale-[1.01] rounded-5xl border-2 border-transparent font-mono text-sm text-black uppercase outline-0 transition-colors focus-visible:border-lime">
-              <div className="absolute top-0 -z-10 h-full w-full rounded-5xl bg-white transition-all duration-500 group-hover/cta:scale-105 group-hover/cta:blur-[2px] hover:bg-black"></div>
-              <div className="flex h-10 items-center gap-2 px-4 text-black">
-                <span>preview orchid</span>
-                <span className="">
-                  <svg
-                    width="17"
-                    height="16"
-                    viewBox="0 0 17 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M8.30724 2.86177C8.56759 2.60142 8.9897 2.60142 9.25005 2.86177L13.9167 7.52843C14.1771 7.78878 14.1771 8.21089 13.9167 8.47124L9.25005 13.1379C8.9897 13.3983 8.56759 13.3983 8.30724 13.1379C8.04689 12.8776 8.04689 12.4554 8.30724 12.1951L11.8358 8.6665H4.11198C3.74379 8.6665 3.44531 8.36803 3.44531 7.99984C3.44531 7.63165 3.74379 7.33317 4.11198 7.33317H11.8358L8.30724 3.80458C8.04689 3.54423 8.04689 3.12212 8.30724 2.86177Z"
-                      fill="currentcolor"
-                    />
-                  </svg>
-                </span>
-              </div>
-            </a>
+            <Button text="preview orchid" />
           </div>
         </section>
 
-        <section>
-          
+        <section id="infoSec1" className="">
+          <div className="relative">
+            <div className="absolute h-full overflow-hidden">
+              <img
+                className="h-full w-full object-cover"
+                src="/img2.webp"
+                alt=""
+              />
+            </div>
+
+            {/* Wrap this with one more Div-Note */}
+            <article className="flex bg-jet px-10 py-8 text-white">
+              <div className="flex min-h-[704px] max-w-[440px] flex-col justify-center gap-10 rounded-[100px] p-12 backdrop-blur-[80px]">
+                <h3 className="heading2 font-serif text-[64px]">
+                  Find <em>what's</em> on <em>your</em> mind
+                </h3>
+
+                <div className="flex flex-col gap-10">
+                  <div className="space-y-4 font-sans text-[18px]">
+                    <p>
+                      Orchid’s advanced intuitive chord generator offers a break
+                      from chord maths and muscle memory and keeps you in the
+                      flow of songwriting.
+                    </p>
+                    <p>
+                      Find a new chord, a new progression, a new voicing, or a
+                      whole new idea.
+                    </p>
+                    <p>Find what’s on your mind. </p>
+                  </div>
+
+                  <Button text="Learn more" />
+                </div>
+              </div>
+            </article>
+          </div>
+        </section>
+
+        <section className="bg-brown">
+          <div className="grid grid-cols-5">
+            {/*  */}
+            <article className="col-span-3 p-6">
+              <div className="overflow-hidden rounded-[100px]">
+                <img className="" src="/img3.png" alt="" />
+              </div>
+            </article>
+            {/*  */}
+
+            <div className="col-span-2 flex max-w-sm flex-col justify-center gap-10 justify-self-center text-white">
+              <h2 className="heading2 font-serif text-[64px]">
+                Easily play an <em>F#minor Jazz</em>
+              </h2>
+              {/*  */}
+              <div className="space-y-10">
+                <div className="space-y-4 font-sans text-[18px]">
+                  <p className="text-balance">
+                    With Orchid you can effortlessly create classic triads and
+                    add diatonically correct extensions without having to rack
+                    your brain for music theory stuff you learned in your teens.
+                  </p>
+                  <p>
+                    Follow the rules of conventional music theory with Key Mode,
+                    or make up your own harmonic rules.
+                  </p>
+                  <p>Just be careful of too much jazz. </p>
+                </div>
+
+                <Button text="learn more" />
+              </div>
+              {/*  */}
+            </div>
+
+            {/*  */}
+          </div>
+        </section>
+
+        <section className="bg-bone">
+          <article className="grid grid-cols-5">
+            {/*  */}
+            <article className="order-1 col-span-3 p-6">
+              <div className="aspect-[61/48] overflow-hidden rounded-[100px]">
+                <img
+                  className="h-full w-full object-cover"
+                  src="/orchid3.webp"
+                  alt=""
+                />
+              </div>
+            </article>
+            {/*  */}
+            <div className="col-span-2 flex max-w-sm flex-col justify-center gap-10 justify-self-center text-black">
+              <h2 className="heading2 font-serif text-[64px]">
+                More than just <em>Arpeggios.</em>
+              </h2>
+              {/*  */}
+              <div className="space-y-10">
+                <div className="space-y-4 font-sans text-[18px]">
+                  <p className="">
+                    Animate your chords with Orchid’s range of performance
+                    settings. Strum or Arpeggiate to bring your chord out note
+                    by note, use pre-programmed Patterns, or try Harp mode for
+                    multi-octave soundscape.
+                  </p>
+                  <p>
+                    Add some Slop for a human touch that would make Dilla proud.
+                  </p>
+                </div>
+
+                <Button3 text="learn more" />
+              </div>
+              {/*  */}
+            </div>
+            {/*  */}
+          </article>
+
+          <article className="grid grid-cols-5">
+            {/*  */}
+            <article className="col-span-3 p-6">
+              <div className="overflow-hidden rounded-[100px]">
+                <img className="h-full w-full" src="/img4.png" alt="" />
+              </div>
+            </article>
+            {/*  */}
+            <div className="col-span-2 flex max-w-sm flex-col justify-center gap-10 justify-self-center text-black">
+              <h2 className="heading2 font-serif text-[64px]">
+                You've got a <em>whole keyboard</em> to play with.
+              </h2>
+              {/*  */}
+              <div className="space-y-10">
+                <div className="space-y-4 font-sans text-[18px]">
+                  <p className="">
+                    Use Orchid’s custom Voicing Dial to expand the single octave
+                    keyboard into multiple octaves. Turn the jog wheel to shift
+                    the position of your chord up and down the keyboard note by
+                    note, creating interesting inversions along the way.
+                  </p>
+                </div>
+
+                <Button3 text="learn more" />
+              </div>
+              {/*  */}
+            </div>
+            {/*  */}
+          </article>
+
+          <article className="grid grid-cols-5">
+            {/*  */}
+            <article className="order-1 col-span-3 p-6">
+              <div className="aspect-[61/48] overflow-hidden rounded-[100px]">
+                <img
+                  className="h-full w-full object-cover"
+                  src="/img5.webp"
+                  alt=""
+                />
+              </div>
+            </article>
+            {/*  */}
+            <div className="col-span-2 flex max-w-sm flex-col justify-center gap-10 justify-self-center text-black">
+              <h2 className="heading2 font-serif text-[64px]">
+                Know what you're playing <em>Or don't</em>
+              </h2>
+              {/*  */}
+              {/* <div className="space-y-10"> */}
+              <div className="space-y-4 font-sans text-[18px]">
+                <p className="">
+                  Ever wanted to know music theory? You’re allowed to want that.
+                  The built-in OLED display shows chord names (and optional note
+                  details) for easy notation and collaboration.
+                </p>
+                <p>
+                  And if you’d rather just focus on the music (and not the
+                  notes) bliss out with some classic 8 bit videos or the onboard
+                  oscilloscope instead.
+                </p>
+              </div>
+              {/* </div> */}
+              {/*  */}
+            </div>
+            {/*  */}
+          </article>
+
+          <article className="grid grid-cols-5">
+            {/*  */}
+            <article className="col-span-3 p-6">
+              <div className="aspect-[61/48] overflow-hidden rounded-[100px]">
+                <img
+                  className="h-full w-full object-cover"
+                  src="/img6.webp"
+                  alt=""
+                />
+              </div>
+            </article>
+            {/*  */}
+            <div className="col-span-2 flex max-w-sm flex-col justify-center gap-10 justify-self-center text-black">
+              <h2 className="heading2 font-serif text-[64px]">
+                Discover your song's <em>destiny</em>.
+              </h2>
+              {/*  */}
+              <div className="space-y-10">
+                <div className="space-y-4 font-sans text-[18px]">
+                  <p className="">
+                    Ever wanted to know music theory? You’re allowed to want
+                    that. The built-in OLED display shows chord names (and
+                    optional note details) for easy notation and collaboration.
+                  </p>
+                  <p>
+                    And if you’d rather just focus on the music (and not the
+                    notes) bliss out with some classic 8 bit videos or the
+                    onboard oscilloscope instead.
+                  </p>
+                </div>
+
+                <Button3 text="Learn more" />
+              </div>
+              {/*  */}
+            </div>
+            {/*  */}
+          </article>
         </section>
       </main>
     </>
---
+++ src/components/Button.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+// Make this button 1 and 2
+
+const Button = ({ text }) => {
+  return (
+    <a href="#" className="flex items-center">
+      <div className="group/cta relative min-w-[120px] scale-[1.01] rounded-5xl border-2 border-transparent font-mono text-sm text-black uppercase outline-0 transition-colors focus-visible:border-lime">
+        <div className="absolute top-0 -z-10 h-full w-full rounded-5xl bg-white transition-all duration-500 group-hover/cta:scale-105 group-hover/cta:blur-[2px] "></div>
+        <div className="flex h-10 items-center gap-2 px-4 text-black">
+          <span>{text}</span>
+          <span className="">
+            <svg
+              width="17"
+              height="16"
+              viewBox="0 0 17 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8.30724 2.86177C8.56759 2.60142 8.9897 2.60142 9.25005 2.86177L13.9167 7.52843C14.1771 7.78878 14.1771 8.21089 13.9167 8.47124L9.25005 13.1379C8.9897 13.3983 8.56759 13.3983 8.30724 13.1379C8.04689 12.8776 8.04689 12.4554 8.30724 12.1951L11.8358 8.6665H4.11198C3.74379 8.6665 3.44531 8.36803 3.44531 7.99984C3.44531 7.63165 3.74379 7.33317 4.11198 7.33317H11.8358L8.30724 3.80458C8.04689 3.54423 8.04689 3.12212 8.30724 2.86177Z"
+                fill="currentcolor"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export const Button2 = ({ text }) => {
+  return (
+    <button className="group/cta relative mx-auto flex min-w-[120px] scale-[1.01] rounded-5xl border-2 border-transparent font-mono text-sm text-black uppercase outline-0 transition-colors focus-visible:border-lime">
+      <div className="absolute top-0 -z-10 h-full w-full rounded-5xl bg-white transition-all duration-500 group-hover/cta:scale-105 group-hover/cta:blur-[2px] "></div>
+      <div className="flex h-10 items-center gap-2 px-4 text-black">
+        <span>{text}</span>
+        <span className="">
+          <svg
+            // width="17"
+            // height="17"
+            viewBox="0 0 17 17"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="aspect-square w-[17px]">
+            <path
+              d="M14.5001 8.89665L3.16675 15.5633V2.22998L14.5001 8.89665Z"
+              fill="currentcolor"
+            />
+          </svg>
+        </span>
+      </div>
+    </button>
+  );
+};
+
+export const Button3 = ({ text }) => {
+  return (
+    <a href="#" className="flex items-center">
+      <div className="group/cta relative min-w-[120px] scale-[1.01] rounded-5xl border-2 border-transparent font-mono text-sm text-black uppercase outline-0 transition-colors focus-visible:border-lime">
+        <div className="absolute top-0 -z-10 h-full w-full rounded-5xl bg-black transition-all duration-500 group-hover/cta:scale-105 group-hover/cta:blur-[2px] "></div>
+        <div className="flex h-10 items-center gap-2 px-4 text-white">
+          <span>{text}</span>
+          <span className="">
+            <svg
+              width="17"
+              height="16"
+              viewBox="0 0 17 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8.30724 2.86177C8.56759 2.60142 8.9897 2.60142 9.25005 2.86177L13.9167 7.52843C14.1771 7.78878 14.1771 8.21089 13.9167 8.47124L9.25005 13.1379C8.9897 13.3983 8.56759 13.3983 8.30724 13.1379C8.04689 12.8776 8.04689 12.4554 8.30724 12.1951L11.8358 8.6665H4.11198C3.74379 8.6665 3.44531 8.36803 3.44531 7.99984C3.44531 7.63165 3.74379 7.33317 4.11198 7.33317H11.8358L8.30724 3.80458C8.04689 3.54423 8.04689 3.12212 8.30724 2.86177Z"
+                fill="currentcolor"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default Button;
